fc0e6133fca237e4d02cd7e022387d1b20000e07
Omar_Jupiter <omarahmed9113@gmail.com>
2023-07-27 14:46:50 +0300

    added script.sh

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git clone https://github.com/Omar-Ahmed-Dt/json-server-app.git
+cd json-server-app
+docker-compose build
+docker-compose up -d
+# wait 60s and check every 1sec 
+timeout 60s bash -c 'while [[ "$(docker-compose ps -q | wc -l)" -ne 3 ]]; do sleep 1; done'
+# print message 
+if [[ "$(docker-compose ps -q | wc -l)" -eq 3 ]]; then
+  echo "Deployment successful! Access the app at http://localhost"
+else
+  echo "Deployment failed! Check the logs for more information."
+fi
